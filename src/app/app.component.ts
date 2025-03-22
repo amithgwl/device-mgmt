ced1dc48b7db3b8e3026f314712282a88df725f0
amithnr <amithnr@hotmail.com>
2025-03-22 12:13:01 +0530

    device activated state and display object details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import {MatListModule} from '@angular/material/list';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatIconModule} from '@angular/material/icon';
-import {CommonModule, DatePipe, NgClass} from '@angular/common';
+import {CommonModule, DatePipe, JsonPipe, NgClass} from '@angular/common';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatCardModule} from '@angular/material/card';
-
-
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatGridListModule} from '@angular/material/grid-list';
 export interface Section {
+  id: Number,
   name: string;
   updated: Date;
   status: Boolean;
@@ -16,27 +18,37 @@ export interface Section {
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatListModule, MatIconModule, MatDividerModule, DatePipe, MatToolbarModule, MatCardModule, NgClass],
+  imports: [RouterOutlet, MatListModule, MatIconModule, MatDividerModule, DatePipe, MatToolbarModule, MatCardModule, NgClass, JsonPipe, MatButtonModule, MatSidenavModule, MatGridListModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'device-mgmt';
+  showFiller = false;
   devices: Section[] = [
     {
+      id: 1,
       name: 'Device 1',
       updated: new Date('1/1/16'),
       status: true
     },
     {
+      id: 2,
       name: 'Device 2',
       updated: new Date('1/17/16'),
       status: false
     },
     {
+      id: 3,
       name: 'Device 3',
       updated: new Date('1/28/16'),
       status: true
     },
   ];
+
+  selectedDevice?: any;
+
+  onSelect(device: any): void {
+    this.selectedDevice = device;
+  }
 }
