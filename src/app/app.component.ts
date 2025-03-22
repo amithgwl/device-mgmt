de5af30881a0dad7d767734b5300c24092a9a76e
amithnr <amithnr@hotmail.com>
2025-03-22 11:14:54 +0530

    added initial list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatListModule} from '@angular/material/list';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+import {DatePipe} from '@angular/common';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+export interface Section {
+  name: string;
+  updated: Date;
+  status: Boolean;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatListModule, MatIconModule, MatDividerModule, DatePipe, MatToolbarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'device-mgmt';
+  devices: Section[] = [
+    {
+      name: 'Photos',
+      updated: new Date('1/1/16'),
+      status: true
+    },
+    {
+      name: 'Recipes',
+      updated: new Date('1/17/16'),
+      status: false
+    },
+    {
+      name: 'Work',
+      updated: new Date('1/28/16'),
+      status: true
+    },
+  ];
 }
