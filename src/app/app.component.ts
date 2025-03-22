e264720a41cce2fc49c1587ab5b2cd22bd39a04b
amithnr <amithnr@hotmail.com>
2025-03-22 13:11:09 +0530

    implement edit and save ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,17 @@ import { RouterOutlet } from '@angular/router';
 import {MatListModule} from '@angular/material/list';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatIconModule} from '@angular/material/icon';
-import {CommonModule, DatePipe, JsonPipe, NgClass} from '@angular/common';
+import {CommonModule, DatePipe, JsonPipe, NgClass, NgIf} from '@angular/common';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatGridListModule} from '@angular/material/grid-list';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+
 export interface Section {
   id: Number,
   name: string;
@@ -18,12 +23,13 @@ export interface Section {
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatListModule, MatIconModule, MatDividerModule, DatePipe, MatToolbarModule, MatCardModule, NgClass, JsonPipe, MatButtonModule, MatSidenavModule, MatGridListModule],
+  imports: [RouterOutlet, MatListModule, MatIconModule, MatDividerModule, DatePipe, MatToolbarModule, MatCardModule, NgClass, JsonPipe, MatButtonModule, MatSidenavModule, MatGridListModule, MatTooltipModule, MatFormFieldModule, MatInputModule, MatSlideToggleModule, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'device-mgmt';
+  editMode: Boolean = false;
   showFiller = false;
   devices: Section[] = [
     {
@@ -47,6 +53,10 @@ export class AppComponent {
   ];
 
   selectedDevice?: any;
+
+  editModeToggle(mode: Boolean) {
+    this.editMode = mode;
+  }
 
   onSelect(device: any): void {
     this.selectedDevice = device;
